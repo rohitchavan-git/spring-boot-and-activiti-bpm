60c5298fbe0e59ff97eaff043f9a61bbaf89f17e
Rohit Chavan <53335793+rohitchavan-git@users.noreply.github.com>
2019-10-01 17:56:40 +0530

    inital file setup

---
+++ activiti-bpm/src/main/java/com/rohitchavan_git/activiti/bpm/ActivitiBpmApplication.java
@@ -0,0 +1,13 @@
+package com.rohitchavan_git.activiti.bpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiBpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiBpmApplication.class, args);
+	}
+
+}
---
+++ activiti-bpm/src/test/java/com/rohitchavan_git/activiti/bpm/ActivitiBpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitchavan_git.activiti.bpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiBpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
